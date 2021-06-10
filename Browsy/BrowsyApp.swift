bfde509e411611de635ed44de292972970a7a262
Marco Romano <marcojulioromano@gmail.com>
2021-06-10 15:28:32 +0200

    MVP
    Implements the basic use case of the app in swift. Though in a very bad way. Will iterate and improve from here.

--- Browsy/BrowsyApp.swift
+++ Browsy/BrowsyApp.swift
@@ -9,9 +9,49 @@ import SwiftUI
 
 @main
 struct BrowsyApp: App {
+    
+    @State private var lastUrl: URL? {
+        didSet {
+            NotificationCenter.default.post(
+                name: NSNotification.Name(rawValue: "lastUrl"),
+                object: nil)
+        }
+    }
+    
+    @State private var browsers: [Bundle] = [Bundle]()
+    
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView(url: lastUrl, browsers: browsers)
+                .onOpenURL(perform: { url in
+                    lastUrl = url
+                    browsers = getInstalledBrowsers()
+                })
         }
+    }
+    
+    func getInstalledBrowsers () -> [ Bundle ] {
+        var browsers = [ Bundle ]()
+        let array = LSCopyAllHandlersForURLScheme("https" as CFString)?
+            .takeRetainedValue()
+        // let array = LSCopyAllRoleHandlersForContentType(
+        //     "public.html" as CFString, LSRolesMask.all)?.takeRetainedValue()
+        for i in 0..<CFArrayGetCount(array!) {
+            let bundleId = unsafeBitCast(
+                CFArrayGetValueAtIndex(array!, i),
+                to: CFString.self
+                ) as String
+            if let path = NSWorkspace.shared
+                .absolutePathForApplication(withBundleIdentifier: bundleId) {
+                if let bundle = Bundle(path: path) {
+                    // let name: String = bundle.infoDictionary!["CFBundleName"] as String
+                    if bundle.bundleIdentifier == Bundle.main.bundleIdentifier {
+                        continue
+                    }
+                    browsers.append(bundle)
+                }
+            }
+        }
+        return browsers
     }
 }
--- Browsy/ContentView.swift
+++ Browsy/ContentView.swift
@@ -8,14 +8,46 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let url: URL?
+    let browsers: [Bundle]
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        let template = "Hello, world! %@"
+        
+        VStack {
+            Text(String(format: template, url?.absoluteString ?? "null"))
+                .padding()
+            List(browsers, id: \.bundleIdentifier) { bundle in
+                HStack {
+                    Text(bundle.bundleIdentifier ?? "none")
+                    Button("OPEN") {
+                        if let urll = url as? URL {
+                            openInSpecificBrowser(url: urll, appId: bundle.bundleIdentifier)
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+    func openInSpecificBrowser(url: URL, appId: String? = nil) -> Bool {
+        return NSWorkspace.shared.open(
+            [url],
+            withAppBundleIdentifier: appId,
+            options: NSWorkspace.LaunchOptions.default,
+            additionalEventParamDescriptor: nil,
+            launchIdentifiers: nil
+        )
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(
+            url: URL(string: "www.apple.com"),
+            browsers: [Bundle]()
+        )
     }
 }
